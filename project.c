10980f1fccd567ab6b995a843b45eac9483bd240
VladConstantinescu02 <126387659+VladConstantinescu02@users.noreply.github.com>
2023-03-28 09:55:41 +0300

    Add files via upload
    W6-fist attempt of the project first part

---
+++ project.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+void printFileProperties(struct stat stats);
+
+int main(int argc, char* argv[]) { 
+
+    char path[50];
+    struct stat stats
+    int status;
+
+
+    
+    return 0;
+}
