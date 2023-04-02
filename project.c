3b0fa40771ac7164e3f8aaee6a73d80f91a857c1
VladConstantinescu02 <126387659+VladConstantinescu02@users.noreply.github.com>
2023-04-02 22:35:02 +0300

    Add files via upload
    Finished the basic implementation for file handling, week 6

--- project.c
+++ project.c
@@ -1,18 +1,54 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
-
-void printFileProperties(struct stat stats);
-
-int main(int argc, char* argv[]) { 
-
-    char path[50];
-    struct stat stats
-    int status;
+#include <time.h>
 
 
+void printFileProperties(char *filename) {    
+
+    struct stat info;
+
+    if(stat(filename, &info) != 0) {
+        perror("stat() error");
+        return;
+    } else {  
     
+    long int size_of_file;
+    
+    printf("\nFile access rights:");
+    if(info.st_mode & R_OK)
+        printf(" read");
+    if(info.st_mode & W_OK)
+        printf(" write");
+    if(info.st_mode & X_OK)
+        printf(" execute\n");
+
+    size_of_file = info.st_size;
+        printf("\nThe size of the file: %ld  bytes",size_of_file);
+        printf("\n");
+
+        printf("Number of hardlinks: %ld", info.st_nlink);
+        printf("\n");
+    }    
+}
+
+void createSymLink(char *file_name, char * link_name) {    
+
+    int sym_link;
+
+    sym_link =symlink(file_name,link_name);
+
+    if(sym_link == 0)
+    printf("Link was created succesfully");
+    }
+
+
+int main() {    
+
+    printFileProperties("ex1.txt");
+
+    createSymLink("ex1.txt", "ex1_link_s");
+
     return 0;
 }
