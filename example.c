d75dbbdb0330064b3120a94cebb3ad4b782c84eb
VladConstantinescu02 <vlad.constantinescu@student.upt.ro>
2023-05-02 00:54:09 +0300

    Some changes from home -H10

---
+++ example.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <time.h>
+
+int main (int atgs, char *argv[]) {
+
+int a = 1, b = 2;
+//int s = 0;
+
+s = a + b;
+
+printf("%d", s);
+
+}
